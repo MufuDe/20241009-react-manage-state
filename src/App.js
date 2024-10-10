4312002b74a359a2b13aa49b3a80c13917a0222c
chendechuan <chen0802@qq.com>
2024-10-10 10:21:07 +0800

    1. 在组件间共享状态；

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
 import { useState } from "react";
 
-export default function Form() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-
-  const fullName = firstName + " " + lastName;
-
-  function handleFirstNameChange(e) {
-    setFirstName(e.target.value);
-  }
-
-  function handleLastNameChange(e) {
-    setLastName(e.target.value);
-  }
-
+export default function Accordion() {
+  const [activeIndex, setActiveIndex] = useState(0);
   return (
     <>
-      <h2>让我们帮你登记</h2>
-      <label>
-        名： <input value={firstName} onChange={handleFirstNameChange} />
-      </label>
-      <label>
-        姓： <input value={lastName} onChange={handleLastNameChange} />
-      </label>
-      <p>
-        你的票将发给：<b>{fullName}</b>
-      </p>
+      <h2>Almaty, Kazakhstan</h2>
+      <Panel
+        title="关于"
+        isActive={activeIndex === 0}
+        onShow={() => setActiveIndex(0)}
+      >
+        阿拉木图人口约200万，是哈萨克斯坦最大的城市。在1929年至1997年之间，它是该国首都。
+      </Panel>
+      <Panel
+        title="词源"
+        isActive={activeIndex === 1}
+        onShow={() => setActiveIndex(1)}
+      >
+        这个名字源于哈萨克语 <span lang="kk-KZ">алма</span>
+        ，是“苹果”的意思，通常被翻译成“满是苹果”。事实上，阿拉木图周围的地区被认为是苹果的祖籍，
+        <i lang="la">Malus sieversii</i> 被认为是目前本土苹果的祖先。
+      </Panel>
     </>
+  );
+}
+
+function Panel({ title, children, isActive, onShow }) {
+  return (
+    <section className="panel">
+      <h3>{title}</h3>
+      {isActive ? <p>{children}</p> : <button onClick={onShow}>显示</button>}
+    </section>
   );
 }
