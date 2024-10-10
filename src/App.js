47fcac3bb46eb9cae4024a9aa191ec58a11bb9ad
chendechuan <chen0802@qq.com>
2024-10-10 10:34:54 +0800

    1. 迁移状态逻辑至 Reducer 中；

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import { useState } from "react";
-import Chat from "./Chat.js";
-import ContactList from "./ContactList.js";
+import { useReducer } from "react";
+import AddTask from "./AddTask.js";
+import TaskList from "./TaskList.js";
 
-export default function Messenger() {
-  const [to, setTo] = useState(contacts[0]);
+export default function TaskApp() {
+  const [tasks, dispatch] = useReducer(tasksReducer, initialTasks);
+
+  function handleAddTask(text) {
+    dispatch({
+      type: "added",
+      id: nextId++,
+      text: text,
+    });
+  }
+
+  function handleChangeTask(task) {
+    dispatch({
+      type: "changed",
+      task: task,
+    });
+  }
+
+  function handleDeleteTask(taskId) {
+    dispatch({
+      type: "deleted",
+      id: taskId,
+    });
+  }
+
   return (
-    <div>
-      <ContactList
-        contacts={contacts}
-        selectedContact={to}
-        onSelect={(contact) => setTo(contact)}
+    <>
+      <h1>布拉格行程</h1>
+      <AddTask onAddTask={handleAddTask} />
+      <TaskList
+        tasks={tasks}
+        onChangeTask={handleChangeTask}
+        onDeleteTask={handleDeleteTask}
       />
-      <Chat key={to.email} contact={to} />
-    </div>
+    </>
   );
 }
 
-const contacts = [
-  { name: "Taylor", email: "taylor@mail.com" },
-  { name: "Alice", email: "alice@mail.com" },
-  { name: "Bob", email: "bob@mail.com" },
+function tasksReducer(tasks, action) {
+  switch (action.type) {
+    case "added": {
+      return [
+        ...tasks,
+        {
+          id: action.id,
+          text: action.text,
+          done: false,
+        },
+      ];
+    }
+    case "changed": {
+      return tasks.map((t) => {
+        if (t.id === action.task.id) {
+          return action.task;
+        } else {
+          return t;
+        }
+      });
+    }
+    case "deleted": {
+      return tasks.filter((t) => t.id !== action.id);
+    }
+    default: {
+      throw Error("未知操作：" + action.type);
+    }
+  }
+}
+
+let nextId = 3;
+const initialTasks = [
+  { id: 0, text: "参观卡夫卡博物馆", done: true },
+  { id: 1, text: "看木偶戏", done: false },
+  { id: 2, text: "列侬墙图片", done: false },
 ];
