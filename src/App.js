507022c622d10689b3e114a72943471d21db55f4
chendechuan <chen0802@qq.com>
2024-10-11 13:50:52 +0800

    1. 使用 Context 深层传递参数；

--- src/App.js
+++ src/App.js
@@ -1,78 +1,25 @@
-import { useReducer } from "react";
-import AddTask from "./AddTask.js";
-import TaskList from "./TaskList.js";
+import Heading from "./Heading.js";
+import Section from "./Section.js";
 
-export default function TaskApp() {
-  const [tasks, dispatch] = useReducer(tasksReducer, initialTasks);
-
-  function handleAddTask(text) {
-    dispatch({
-      type: "added",
-      id: nextId++,
-      text: text,
-    });
-  }
-
-  function handleChangeTask(task) {
-    dispatch({
-      type: "changed",
-      task: task,
-    });
-  }
-
-  function handleDeleteTask(taskId) {
-    dispatch({
-      type: "deleted",
-      id: taskId,
-    });
-  }
-
+export default function Page() {
   return (
-    <>
-      <h1>布拉格行程</h1>
-      <AddTask onAddTask={handleAddTask} />
-      <TaskList
-        tasks={tasks}
-        onChangeTask={handleChangeTask}
-        onDeleteTask={handleDeleteTask}
-      />
-    </>
+    <Section>
+      <Heading>大标题</Heading>
+      <Section>
+        <Heading>一级标题</Heading>
+        <Heading>一级标题</Heading>
+        <Heading>一级标题</Heading>
+        <Section>
+          <Heading>二级标题</Heading>
+          <Heading>二级标题</Heading>
+          <Heading>二级标题</Heading>
+          <Section>
+            <Heading>三级标题</Heading>
+            <Heading>三级标题</Heading>
+            <Heading>三级标题</Heading>
+          </Section>
+        </Section>
+      </Section>
+    </Section>
   );
 }
-
-function tasksReducer(tasks, action) {
-  switch (action.type) {
-    case "added": {
-      return [
-        ...tasks,
-        {
-          id: action.id,
-          text: action.text,
-          done: false,
-        },
-      ];
-    }
-    case "changed": {
-      return tasks.map((t) => {
-        if (t.id === action.task.id) {
-          return action.task;
-        } else {
-          return t;
-        }
-      });
-    }
-    case "deleted": {
-      return tasks.filter((t) => t.id !== action.id);
-    }
-    default: {
-      throw Error("未知操作：" + action.type);
-    }
-  }
-}
-
-let nextId = 3;
-const initialTasks = [
-  { id: 0, text: "参观卡夫卡博物馆", done: true },
-  { id: 1, text: "看木偶戏", done: false },
-  { id: 2, text: "列侬墙图片", done: false },
-];
