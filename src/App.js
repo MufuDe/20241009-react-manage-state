2e0d998fb49f6b5fa49d70bc94f16f1ce4b6fc5c
chendechuan <chen0802@qq.com>
2024-10-14 15:07:41 +0800

    1. 返回两个单独的 JSX 代码块；

--- src/App.js
+++ src/App.js
@@ -2,25 +2,25 @@ import { useState } from "react";
 
 export default function Picture() {
   const [isActive, setIsActive] = useState(false);
-
-  let backgroundClassName = "background";
-  let pictureClassName = "picture";
   if (isActive) {
-    pictureClassName += " picture--active";
-  } else {
-    backgroundClassName += " background--active";
+    return (
+      <div className="background" onClick={() => setIsActive(false)}>
+        <img
+          className="picture picture--active"
+          alt="Rainbow houses in Kampung Pelangi, Indonesia"
+          src="https://i.imgur.com/5qwVYb1.jpeg"
+          onClick={(e) => e.stopPropagation()}
+        />
+      </div>
+    );
   }
-
   return (
-    <div className={backgroundClassName} onClick={() => setIsActive(false)}>
+    <div className="background background--active">
       <img
-        onClick={(e) => {
-          e.stopPropagation();
-          setIsActive(true);
-        }}
-        className={pictureClassName}
+        className="picture"
         alt="Rainbow houses in Kampung Pelangi, Indonesia"
-        src="/img/5qwVYb1.jpg"
+        src="https://i.imgur.com/5qwVYb1.jpeg"
+        onClick={() => setIsActive(true)}
       />
     </div>
   );
