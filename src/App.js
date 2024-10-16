57e92eb62e14b76e607b0bd24801f9e9b68915c7
chendechuan <chen0802@qq.com>
2024-10-16 15:40:00 +0800

    1. 修复丢失的输入框文本;

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import { useState } from "react";
-import Chat from "./Chat.js";
-import ContactList from "./ContactList.js";
 
-export default function Messenger() {
-  const [to, setTo] = useState(contacts[0]);
+export default function App() {
+  const [showHint, setShowHint] = useState(false);
+  if (showHint) {
+    return (
+      <div>
+        <p>
+          <i>提示：你最喜欢的城市？</i>
+        </p>
+        <Form />
+        <button
+          onClick={() => {
+            setShowHint(false);
+          }}
+        >
+          隐藏提示
+        </button>
+      </div>
+    );
+  }
   return (
     <div>
-      <ContactList
-        contacts={contacts}
-        selectedContact={to}
-        onSelect={(contact) => setTo(contact)}
-      />
-      <Chat key={to.id} contact={to} />
+      <Form />
+      <button
+        onClick={() => {
+          setShowHint(true);
+        }}
+      >
+        显示提示
+      </button>
     </div>
   );
 }
 
-const contacts = [
-  { id: 0, name: "Taylor", email: "taylor@mail.com" },
-  { id: 1, name: "Alice", email: "alice@mail.com" },
-  { id: 2, name: "Bob", email: "bob@mail.com" },
-];
+function Form() {
+  const [text, setText] = useState("");
+  return <textarea value={text} onChange={(e) => setText(e.target.value)} />;
+}
