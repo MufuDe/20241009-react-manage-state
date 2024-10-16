09c8634161480c6851346a25381fb114068fdb4a
chendechuan <chen0802@qq.com>
2024-10-16 14:14:12 +0800

    1. 实现多选功能；

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
 import { useState } from "react";
-import { initialLetters } from "./data.js";
+import { letters } from "./data.js";
 import Letter from "./Letter.js";
 
 export default function MailClient() {
-  const [letters, setLetters] = useState(initialLetters);
-  const [highlightedId, setHighlightedId] = useState(null);
+  const [selectedId, setSelectedId] = useState(null);
 
-  function handleHover(letterId) {
-    setHighlightedId(letterId);
-  }
+  // TODO: 支持多选
+  const selectedCount = 1;
 
-  function handleStar(starredId) {
-    setLetters(
-      letters.map((letter) => {
-        if (letter.id === starredId) {
-          return {
-            ...letter,
-            isStarred: !letter.isStarred,
-          };
-        } else {
-          return letter;
-        }
-      })
-    );
+  function handleToggle(toggledId) {
+    // TODO: 支持多选
+    setSelectedId(toggledId);
   }
 
   return (
@@ -33,11 +21,17 @@ export default function MailClient() {
           <Letter
             key={letter.id}
             letter={letter}
-            isHighlighted={letter.id === highlightedId}
-            onHover={handleHover}
-            onToggleStar={handleStar}
+            isSelected={
+              // TODO: 支持多选
+              letter.id === selectedId
+            }
+            onToggle={handleToggle}
           />
         ))}
+        <hr />
+        <p>
+          <b>You selected {selectedCount} letters</b>
+        </p>
       </ul>
     </>
   );
